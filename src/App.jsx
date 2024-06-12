11be405604e002f8b046a53deb0ec5e9b80bb485
KennedyRyan-code <kenryan49@gmail.com>
2024-06-12 15:28:03 +0300

    new file:   src/assets initial app setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">Campaign Portfolio !</h1>
+
+        <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+          <Button>Button</Button>
+        </div>
+        <ButtonGradient />
+      </div>
+    </>
+  );
+};
+
+export default App;
