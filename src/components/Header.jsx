878b1aa5a66806ade877c22802cee566577c882b
KennedyRyan-code <kenryan49@gmail.com>
2024-11-07 17:00:28 +0300

    Added a nav tag and imported the navigation list from constants

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import brainwave from "../assets/brainwave.svg";
+import { navigation } from "../constants/";
 
 const Header = () => {
   return (
@@ -8,6 +9,22 @@ const Header = () => {
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={70} alt="logo" />
         </a>
+
+        <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+            {navigation.map((item) => (
+              <a
+                key={item.id}
+                href={item.url}
+                className={`block relative font-code text-2xl uppercase tex-n-1 transition-colors hover:text-color-1 ${
+                  item.onlyMobile ? "lg:hidden" : ""
+                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold`}
+              >
+                {item.title}
+              </a>
+            ))}
+          </div>
+        </nav>
       </div>
     </div>
   );
