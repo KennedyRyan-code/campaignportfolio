3b5ddc758406d8bf4abcc6223fd845d555f552d0
KennedyRyan-code <kenryan49@gmail.com>
2024-11-07 17:08:11 +0300

    added pathname

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import brainwave from "../assets/brainwave.svg";
 import { navigation } from "../constants/";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+  const pathname = useLocation();
+
   return (
     <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4 gap-x-10">
@@ -18,7 +21,11 @@ const Header = () => {
                 href={item.url}
                 className={`block relative font-code text-2xl uppercase tex-n-1 transition-colors hover:text-color-1 ${
                   item.onlyMobile ? "lg:hidden" : ""
-                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold`}
+                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                  item.url === pathname.hash
+                    ? "z-2 lg:text-n-1"
+                    : "lg:text-n-1/50"
+                }`}
               >
                 {item.title}
               </a>
