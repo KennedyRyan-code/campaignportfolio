f9942f2518449dfa0952cefffd60fdb49853ab8b
KennedyRyan-code <kenryan49@gmail.com>
2024-11-08 21:31:12 +0300

    Added the inital file for Section component

---
+++ src/components/Section.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Section = ({
+  className,
+  id,
+  crosses,
+  crossesOffset,
+  customPaddings,
+  children,
+}) => {
+  return <div className={``}>{children}</div>;
+};
+
+export default Section;
