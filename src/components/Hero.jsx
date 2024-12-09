98f004c6f04845e967dcd668b81230ad246af057
KennedyRyan-code <kenryan49@gmail.com>
2024-12-09 13:59:24 +0300

    transforming to cosmic content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,9 +23,9 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
+            Transforming Ideas into Digital Masterpieces {` `}
             <span className="inline-block relative">
-              Brainwave{" "}
+              &nbsp;Cosmic Tech&nbsp;{" "}
               <img
                 src={curve}
                 className="absolute top-full left-0 w-full xl:-mt-2"
@@ -36,12 +36,10 @@ const Hero = () => {
             </span>
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-            Unleash the power of AI within CosmicFlow. Upgrade your productivity
-            with Brainwave, the open AI chat app.
+            Building Solutions that Inspire, Elevate, and Endure! &nbsp; Bundled
+            with search engine optimization, web security, responsive design.
           </p>
-          <Button href="/pricing" white>
-            Get started
-          </Button>
+          <Button href="/pricing">Get started</Button>
         </div>
         <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
           <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
@@ -54,7 +52,7 @@ const Hero = () => {
                   className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
                   width={1024}
                   height={490}
-                  alt="AI"
+                  alt="space"
                 />
 
                 <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
