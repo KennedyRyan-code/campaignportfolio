e830a38ef47042717c25c2d97c84afde808b4642
KennedyRyan-code <kenryan49@gmail.com>
2024-12-13 13:32:49 +0300

    added content and new icons

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,4 +1,4 @@
-import { brainwaveSymbol, check } from "../assets";
+import { brainwaveSymbol, check, mobilelogo } from "../assets";
 import { collabApps, collabContent, collabText } from "../constants";
 import Button from "./Button";
 import Section from "./Section";
@@ -36,12 +36,12 @@ const Collaboration = () => {
           <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
             <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
               <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
-                <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
+                <div className="flex items-center justify-center w-full h-full bg-neutral-300 rounded-full">
                   <img
-                    src={brainwaveSymbol}
-                    width={48}
-                    height={48}
-                    alt="brainwave"
+                    src={mobilelogo}
+                    width={78}
+                    height={78}
+                    alt="Cosmictech"
                   />
                 </div>
               </div>
